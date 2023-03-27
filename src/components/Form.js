05ae7c178519e3b707fdc99df188330056e8a3b1
MosesBonilla <bonillamoses@gmail.com>
2023-03-26 22:35:38 -0400

    gonig a little crazy...

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,8 +5,15 @@ const Form = () => {
     /*
             TODO - Logic for changing state based on form changes
         */
+       const handleURLCh=(event)=>{
+        setURLcord(event.target.value)
+       }
+       const handleURLname=(event)=>{
+        setURLname(event.target.value)
+       }
   }
-
+const [urlName, setURLname]= useState(``)
+const [urlCord, setURLcord]= useState(``)
   const onFormSubmit = (event) => {
     // to prevent page reload on form submit
     event.preventDefault()
@@ -15,12 +22,37 @@ const Form = () => {
     /*
             TODO - Logic for calling props to handle submission and setting state changes
         */
+    const handleURLcord=(event)=>{
+      console.log(event)
+      setURLcord(event.target.value)
+    }
+    const handleURLname=(event)=>{
+      setURLname(event.target.value)
+    }
   }
 
   return (
-    <form>
-      {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
-    </form>
+    <form onSubmit={onFormSubmit}>
+      <label>
+         Name: 
+        <input
+        type='text'
+        id='urlName'
+        name='urlName'
+        onChange={handleChange}/>
+      </label>
+      
+      <label>
+         URL: 
+        <input
+        type='text'
+        id='urlCord'
+        name='urlCord'
+        onChange={handleChange}/>
+      </label>
+      <input type="submit" value="Submit"/>
+       
+    </form>//TODO - Logic for returning a form element with labels and inputs for link name and URL
   )
 }
 
--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -19,12 +19,12 @@ const LinkContainer = (props) => {
     <div className="container">
       <h1>My Favorite Links</h1>
       <p>Add a new url with a name and link to the table.</p>
-      {/*TODO - Add Table Component */}
+      {<Table /> /*TODO - Add Table Component */}
 
       <br />
 
       <h3>Add New</h3>
-      {/*TODO - Add Form Component */}
+      {<Form /> /*TODO - Add Form Component */}
     </div>
   )
 }
